c1d2467875f3fab2cf888ca3ea90cebc91dace5d
Zain ul abdeen <iamzainulabdeen@gmail.com>
2020-05-21 05:16:13 +0200

    Added first neuron and layer

---
+++ 1. Neuron.py
@@ -0,0 +1,16 @@
+
+
+#Coding simple neuron
+# A simply neuron is consist of some inputs multiplied by weights and an added bias
+#Every input has a different attached to it
+
+inputs = [1.2,2,3,4,5]
+weights = [2,3,1]
+bias = 2
+
+#Output of neuron
+
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
+#Every neuron has only one ouput
+#This output can be sent to many neurons in next layer
